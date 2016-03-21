55ec4aef15e7ebb1863e8ce3285e602e78e9aa18
karolsojko <karolsojko@gmail.com>
2016-03-21 14:42:01 +0100

    Added uploading to S3

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var fs = require('fs');
 var firebase = require('firebase');
+var AWS = require('aws-sdk');
 
-var firebaseRef = new Firebase('https://unleash-app-staging.firebaseio.com/');
+var firebaseRef = new Firebase(process.env.FIREBASE_URL);
 
-firebaseRef.authWithCustomToken('0NFoMPLS9qPgmsYV5GRtPH5a6d0UW1tWwu3zimyu', function (error, authData) {
+firebaseRef.authWithCustomToken(process.env.FIREBASE_TOKEN, function (error, authData) {
   if (error) {
     console.log(error);
   }
@@ -12,12 +12,18 @@ firebaseRef.authWithCustomToken('0NFoMPLS9qPgmsYV5GRtPH5a6d0UW1tWwu3zimyu', func
     var now = new Date();
     var filename = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + '-' + now.getTime() + '.json';
 
-    fs.writeFile('data/' + filename, JSON.stringify(snapshot.exportVal()), function (error) {
-      if (error) {
-        console.log(error);
+    var s3bucket = new AWS.S3({
+      params: {
+        Bucket: process.env.S3_BUCKET,
+        Key: filename
+      }
+    });
+
+    s3bucket.upload({Body: JSON.stringify(snapshot.exportVal())}, function(err, data) {
+      if (err) {
+        console.log("Error uploading data: ", err);
       } else {
-        console.log('Saved succesfully!');
-        process.exit();
+        console.log("Successfully uploaded data to S3");
       }
     });
   }, function (errorObject) {
